99d11b11d8e6027328a2e243319ad9e39a58a68e
M BHANU TEJA <bhanuteja2696@gmail.com>
2018-07-24 22:46:55 +0530

    Update settings.py

--- BookMyMovieProject/settings.py
+++ BookMyMovieProject/settings.py
@@ -132,6 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bhanuteja5327@gmail.com'
+EMAIL_HOST_USER = 'bookmymovie619@gmail.com'
 EMAIL_HOST_PASSWORD = 'sbhbhanu'
 EMAIL_USE_TLS = True
